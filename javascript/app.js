1701acba1b091b49115cd5a4b0a6b3f18a8c4ee7
JoshCantor <josh.s.cantor@gmail.com>
2015-11-18 14:30:23 -0800

    created start button to instatiate game. When clicked, prompts user to input a number of players, and then that number of players is generated

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,4 @@
-// var createPlayer = function() {
+var createGamePlayer = function() {
 	var playerContainer = $('<div>').addClass('player');
 
 	var playerImg = $('<img>').attr('src', 'images/card_back.png');
@@ -7,7 +7,32 @@
 
 	playerContainer.append(playerImg, playerName);
 
-	$('.row').append(playerContainer);
-// 	return playerContainer;
-// }
+	$('.players').append(playerContainer);
+	
+	return playerContainer;
+}
 
+var createAllPlayers = function(number) {
+	for (i = 0; i < number; i++) {
+		console.log(i);
+		createGamePlayer();
+	}
+}
+
+var createStartButton = function() {
+	var buttonContainer = $('<div>').addClass('col-md-3 buttonContainer')
+	$('.buttons').append(buttonContainer);
+	var startButton = $('<a>').addClass('btn btn-info startButton').html('Click to start!');
+	$('.buttonContainer').append(startButton);
+}
+createStartButton();
+
+var startGameButton = function() {
+	$('.startButton').on('click', function() {
+		var numberOfPlayers = window.prompt("How many players woudld you like?");
+		createAllPlayers(numberOfPlayers);
+		var gameStart = new Game(numberOfPlayers);
+
+	})
+}
+startGameButton();
